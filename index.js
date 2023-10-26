68819798f5ad19b354deb9acffda2255e67b111d
asmsadat <personal.asmsadat@gmail.com>
2023-10-26 11:51:09 +0600

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cwcWinner=require('./schema')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cwcWinner=require('./schema');
 
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 3000;
 
 mongoose.connect('mongodb://127.0.0.1:27017/IccWccWinner')
 .then(()=> console.log("Connection Successful"))
@@ -12,25 +14,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/IccWccWinner')
 
 app.get('/api', async(req, res) => {
     try{
-        const winners = await cwcWinner.find({})
-        res.status(200).json({winners})
+        const winners = await cwcWinner.find({});
+        res.status(200).json({winners});
     }
     catch{
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
   })
 
 app.get('/api/:id', async(req,res) => {
     try{
-        const {id}=req.params
-        const winner = await cwcWinner.findById(id)
-        res.status(200).json({winner})
+        const {id}=req.params;
+        const winner = await cwcWinner.findById(id);
+        res.status(200).json({winner});
     }
     catch{
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
 })
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`)
+    console.log(`Running on port ${port}`);
   })
